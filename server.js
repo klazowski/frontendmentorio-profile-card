431aba8c2905047e6ea64790191f2fb8c8655b1e
klazowski <klazowski+gitub@gmail.com>
2021-04-01 20:37:10 +0200

    Add static servind for other files

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-"use strict";
-const { response } = require('express');
+'use strict';
 var express = require('express');
+var path = require('path');
+var app = express();
 
-var app = express()
+const root = path.resolve(__dirname);
 
-app.get('*', (req, res) => {
-  response.sendFile('index.html', __dirname)
-})
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root });
+});
 
-app.listen(5003, () => console.log("Server started at port 5003."));
+app.get('/:filename', (req, res) => {
+  res.sendFile(req.params.filename, { root });
+});
+
+app.get('/images/:filename', (req, res) => {
+  res.sendFile(req.params.filename, { root: path.resolve(root, 'images') });
+});
+
+app.listen(5003, () => console.log('Server started at port 5003.'));
